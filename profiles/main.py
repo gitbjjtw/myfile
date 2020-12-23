6d2bc6f11e8e3906753805a79830d66c1c3fcfbf
gitbjjtw <bjjtw@protonmail.com>
2020-12-23 13:36:55 +0800

    send msg to tg

--- profiles/main.py
+++ profiles/main.py
@@ -1,7 +1,12 @@
 import reqeusts
+import os
 
 
 def send_msg_to_me(msg):
+    CHAT_ID = os.environ['CHAT_ID']
+    TG_BOT_TOKEN = os.environ['TG_BOT_TOKEN']
+    BOT_MSG_URL = 'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'.format(BOT_TOKEN=TG_BOT_TOKEN)
+
     r = requests.post(BOT_MSG_URL, data={'chat_id': CHAT_ID, 'text': msg})
     return r
 
